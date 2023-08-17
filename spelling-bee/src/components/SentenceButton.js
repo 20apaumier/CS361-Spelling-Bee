3a43b8b882f67d738cfeb77885522bae66dc4983
20apaumier <39337835+20apaumier@users.noreply.github.com>
2023-08-17 07:45:04 -0700

    removed code smells

--- spelling-bee/src/components/SentenceButton.js
+++ spelling-bee/src/components/SentenceButton.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 function SentenceButton({ sentence }) {
     const playAudio = () => {
-        var msg = new SpeechSynthesisUtterance();
+        const msg = new SpeechSynthesisUtterance();
         msg.text = sentence;
         window.speechSynthesis.speak(msg);
     };
