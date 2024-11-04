9b03d045151bfe5797db35179a546e16934ddcc9
AJ Paumier <39337835+20apaumier@users.noreply.github.com>
2024-11-03 21:12:26 -0800

    new branch for heroku deployments, only contains server

--- server/routes/statsRoutes.js
+++ server/routes/statsRoutes.js
@@ -7,7 +7,7 @@ const { updateWordStats, updateGameStats, getTopUsers, getUserStats } = require(
 router.use(
     cors({
         credentials: true,
-        origin: 'cs-361-spelling-ku55cwei3-ajs-projects-178b9a17.vercel.app'
+        origin: 'https://spelling-bee-aj-2657ad9a4e22.herokuapp.com/'
     })
 );
 
