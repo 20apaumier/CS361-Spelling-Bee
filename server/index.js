645c313281889551d62c19e1f14580d12b013c44
AJ Paumier <39337835+20apaumier@users.noreply.github.com>
2024-11-09 21:13:22 -0800

    updated origin url

--- server/index.js
+++ server/index.js
@@ -15,7 +15,7 @@ app.use(express.json())
 app.use(cookierParser())
 app.use(express.urlencoded({extended: false}))
 app.use(cors({
-    origin: "https://20apaumier.github.io",
+    origin: "https://20apaumier.github.io/CS361-Spelling-Bee/",
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true
