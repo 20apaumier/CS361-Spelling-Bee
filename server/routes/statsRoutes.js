85cc15312f8492f151871ed7d163c1d6b16122ce
AJ Paumier <39337835+20apaumier@users.noreply.github.com>
2024-11-09 21:06:36 -0800

    removed middleware

--- server/routes/statsRoutes.js
+++ server/routes/statsRoutes.js
@@ -3,14 +3,6 @@ const router = express.Router();
 const cors = require('cors');
 const { updateWordStats, updateGameStats, getTopUsers, getUserStats } = require('../controllers/statsController')
 
-// middleware
-router.use(
-    cors({
-        credentials: true,
-        origin: 'https://spelling-bee-aj-2657ad9a4e22.herokuapp.com/'
-    })
-);
-
 router.post('/updateWordStats', updateWordStats)
 router.post('/updateGameStats', updateGameStats)
 router.get('/getTopUsers', getTopUsers)
